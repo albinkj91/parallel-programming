9750b637fe7657f2da0819049d290a9fa2121d41
Oliver Specht <oliver.specht@tum.de>
2025-09-25 10:27:39 +0200

    task 1 done

--- assignment2/run_task1_equally.sh
+++ assignment2/run_task1_equally.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
-for (( n=1; n<100000000; n*=2 )) ; {
+for (( n=1; n<1000000000; n*=2 )) ; {
    echo "n: " $n
    for (( t=1; t<128; t*=2)) ; {
+      echo -n "Threads: " $t " "
       ./numerical_integration $t $n
    }
 }
