503a646e54630a246d4e2d5a2a1814e92fb95571
Oliver Specht <oliver.specht@tum.de>
2025-09-25 10:44:55 +0200

    New file structure dropped

--- assignment2/coarse-grained.cpp
+++
@@ -1,6 +0,0 @@
-#include "sorted_list.hpp"
-
-void sorted_list::insert(T v)
-{
-    int i{0};
-}
--- assignment2/run_task1_ticketing.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-for (( n=8; n<=64; n*=2)) ; {
-   echo "With " $n " threads:"
-   for (( t=1; t<=67108864; t*=2)) ; {
-      echo -n "Segment size: " $t " "
-      ./numerical_integration $n 67108864 -t $t
-   }
-}
